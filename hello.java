001cc82c5bc080fc132a7f2b6a22f18cd93a95d4
Mikelarbi4u1 <128620484+Mikelarbi4u1@users.noreply.github.com>
2023-03-22 11:53:03 -0400

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,14 +1,14 @@
-// a simple program to practice common Github commands 
+// a simple program to practice common Github commands
 
 public class hello {
 
     public static void main(String [] args) {
 
        // initialize variables
-       String firstName = "Alex";
-	
+       String firstName = "Michael";
+ System.out.println ("Hello My name is " + firstName);
+
        // Using the value of the variable above, output a message in the format, "Hello, I am Alex".
 
     }
 }
-
